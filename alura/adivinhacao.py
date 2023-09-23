889c944bc942d8e1823fe587de9f6ff068bdaa2e
adriakms <adriakms@gmail.com>
2023-09-23 12:48:23 -0300

    modified /alura/adivinhacao.py

--- alura/adivinhacao.py
+++ alura/adivinhacao.py
@@ -1,12 +1,11 @@
 print("**********\nBem vindo ao jogo de adivinhação!\n**********")
 
 numero_secreto = 42
-
 chute = input("Digite o seu número: ")
 
 print("Você digitou ", chute)
 
-if (numero_secreto == int(chute)):
+if numero_secreto == int(chute):
     print ("Você acertou!")
 else:
     print ("Você errou!")
