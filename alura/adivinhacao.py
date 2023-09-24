d6360c179c31c9e73674f40a90fde3a30ced33b9
adriakms <adriakms@gmail.com>
2023-09-24 10:21:27 -0300

    modified /alura/adivinhacao.py

--- alura/adivinhacao.py
+++ alura/adivinhacao.py
@@ -1,11 +1,23 @@
-print("**********\nBem vindo ao jogo de adivinhação!\n**********")
+print("*********************************\nBem vindo ao jogo de adivinhação!\n*********************************")
 
 numero_secreto = 42
-chute = input("Digite o seu número: ")
+total_tentativas = 3
+rodada = 1
 
-print("Você digitou ", chute)
+while rodada <= total_tentativas:
+    print("Tentativa", rodada, "de", total_tentativas)
+    chute_str = input("Digite o seu número:")
+    print("Você digitou: ", chute_str)
+    chute = int(chute_str)
 
-if numero_secreto == int(chute):
-    print ("Você acertou!")
-else:
-    print ("Você errou!")
+    if chute == numero_secreto:
+        print("Você acertou!")
+    else:
+        if chute > numero_secreto:
+            print("Você errou! O seu chute foi maior do que o número secreto.")
+        elif chute < numero_secreto:
+            print("Você errou! O seu chute foi menor do que o número secreto.")
+
+    rodada = rodada + 1
+    
+print("Fim do jogo.")
